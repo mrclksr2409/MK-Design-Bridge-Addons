7109798aa132791b0d90199a0adac98aa89b6e4f
mrclksr2409 <51036118+mrclksr2409@users.noreply.github.com>
2019-10-25 09:31:15 +0200

    update to git sever

--- function.php
+++ function.php
@@ -8,24 +8,25 @@ Author URI: https://www.your-own-design.de/
 */
 
 include_once('inc/icon_text.php');
-include_once('inc/page_box.php'); 
+
+include_once('inc/page_box.php');
 
 include_once('inc/blog_box.php');
 include_once('inc/blog_overlay_box.php');
 
-include_once('inc/portfolio_widget.php');
 include_once('inc/portfolio_box.php');
+include_once('inc/portfolio_widget.php');
 include_once('inc/portfolio_list.php');
 
 include_once('inc/locations.php');
 include_once('inc/locations-rest-api.php');
 
 // update plugin:
-require 'plugin-update-checker.php';
+require 'plugin-update-checker/plugin-update-checker.php';
 $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-	'http://wp-plugins.your-own-design.de/mk-design-bridge-addons/plugin.json',
-	__FILE__, //Full path to the main plugin file or functions.php.
-	'PTCAddons'
+	'https://github.com/mrclksr2409/MK-Design-Bridge-Addons',
+	__FILE__,
+	'MKDesignBridgeAddons'
 );
 
 // Register the style like this for a plugin:
